eb0054c899730be42da4224918dce385c2125c82
Monique Dias santos <moniquediassantos@gmail.com>
2022-04-14 15:30:53 -0300

    todo challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -11,19 +11,39 @@ interface Task {
 }
 
 export function TaskList() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [isComplete, setIsComplete]= useState(false);
+  const [tasks, setTasks] = useState<Task[]>([]); 
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+function handleCreateNewTask() {
+   if (!newTaskTitle) 
+   return;
+
+   const newTask = {
+     id: Math.random(),
+     title: newTaskTitle,
+     isComplete: false
+   }
+   setTasks(tasks => [...tasks, newTask]);
+   setNewTaskTitle('');
+    
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+  function handleToggleTaskCompletion(id:number ) {
+    const isChecked = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    setTasks(isChecked);
+    
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filterTaskId = tasks.filter(task => task.id !== id)
+    setTasks(filterTaskId);
   }
 
   return (
